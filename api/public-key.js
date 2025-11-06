a2a9d2b47adb637f1555fd67d6623012912ebaa0
jkovaly <jkovalycsuk@gmail.com>
2025-11-06 21:22:56 +0100

    Update public-key.js

--- api/public-key.js
+++ api/public-key.js
@@ -1,3 +1,3 @@
-export default function handler(req, res) {
-  return res.status(200).json({ publicKey: process.env.VAPID_PUBLIC || "" });
-}
+module.exports = (req, res) => {
+  res.status(200).json({ publicKey: process.env.VAPID_PUBLIC || "" });
+};
